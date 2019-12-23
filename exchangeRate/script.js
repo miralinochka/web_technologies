f3aee474606c152969d2be712f703af57c6c3eac
Alina Mironchik <alina.mironchik@softensity.com>
2019-12-23 23:03:11 +0300

    Rewrite with jquery"

--- exchangeRate/script.js
+++ exchangeRate/script.js
@@ -1,37 +1,28 @@
 const currencyImg = new Image();
 currencyImg.src = "../images/currency.jpg";
 
-window.onload = () => {
-  const datePicker = document.getElementById('datePicker');
-  const leftBlock = document.getElementById('left');
-  const loader = document.querySelector('.loader');
-  
-  datePicker.max = new Date().toISOString().split("T")[0];
-  loader.className = '';
+$(function() {  
+  $('#datePicker').max = new Date().toISOString().split("T")[0];
+  $('.loader').removeClass('loader');
 
-  leftBlock.append(currencyImg);
-}
+  $('#left').append(currencyImg);
+})
 
 const getCurrenciesByDate = event => {
   event.preventDefault();
 
-  const xhttp = new XMLHttpRequest();
-  const date = document.getElementById('datePicker').value;
-  const loader = document.getElementById('loader');
-  loader.className = 'loader';
-  
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      const resultDiv = document.getElementById("result");
-      const response = JSON.parse(this.responseText);
+  const date = $('#datePicker').val();
+  const resultDiv = $('#result');
+  const loader = $('#loader');
+  loader.addClass('loader');
+  resultDiv.html('');
 
-      const result = response.map(day => {
-        return `<p>${day.Cur_Scale} ${day.Cur_Name}(${day.Cur_Abbreviation}) = ${day.Cur_OfficialRate}</p>`
-      })
-      loader.className = '';
-      resultDiv.innerHTML = result.join('');
-    }
-  };
-  xhttp.open("GET", `https://www.nbrb.by/api/exrates/rates?ondate=${date}&periodicity=0`, true);
-  xhttp.send();
+  $.get(`https://www.nbrb.by/api/exrates/rates?ondate=${date}&periodicity=0`, function(response, status){
+    const result = response.map(day => {
+      return `<p>${day.Cur_Scale} ${day.Cur_Name}(${day.Cur_Abbreviation}) = ${day.Cur_OfficialRate}</p>`
+    })
+    loader.removeClass('loader');
+    resultDiv.html(result.join(''));
+  });
+
 }
